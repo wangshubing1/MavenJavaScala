704a5e3627ace99e76f64b5cb4e6b6434cdefbf6
wangshubing1 <wang_shubing@126.com>
2019-07-19 10:32:56 +0800

    init

---
+++ src/main/java/Test01.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: king
+ * @Date: 2019-07-19
+ * @Desc: TODO
+ */
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+    }
+}
